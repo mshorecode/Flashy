b7cf3018ee99e705e9c72379492ae72fddf15384
Ryan Shore <mshorecode@gmail.com>
2024-05-20 18:32:25 -0500

    setup CRD set API calls

--- API/SetAPI.cs
+++ API/SetAPI.cs
@@ -9,7 +9,31 @@ namespace Flashy.API
     {
         public static void Map(WebApplication app)
         {
+            app.MapPost("/sets", async (FlashyDbContext db, Set flashcardSet) =>
+            {
+                await db.Sets.AddAsync(flashcardSet);
+                await db.SaveChangesAsync();
+                return Results.Created($"/sets/{flashcardSet.Id}", flashcardSet);
+            });
 
+            app.MapGet("/sets", async (FlashyDbContext db) =>
+            {
+                var sets = await db.Sets.ToListAsync();
+                return Results.Ok(sets);
+            });
+
+            app.MapDelete("/sets/{id}", async (FlashyDbContext db, int id) =>
+            {
+                var flashcardSet = await db.Sets.FindAsync(id);
+                if (flashcardSet == null)
+                {
+                    return Results.NotFound();
+                }
+
+                db.Sets.Remove(flashcardSet);
+                await db.SaveChangesAsync();
+                return Results.NoContent();
+            });
         }
     }
 }
