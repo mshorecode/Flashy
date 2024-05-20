00ffeea43773fddc8506a71a49dac66b9adddf0e
Ryan Shore <mshorecode@gmail.com>
2024-05-20 15:53:06 -0500

    setup checkuser and register API calls

--- API/UserAPI.cs
+++ API/UserAPI.cs
@@ -1,4 +1,5 @@
-﻿using Flashy.Models;
+﻿using Flashy.DTOs;
+using Flashy.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
@@ -9,6 +10,35 @@ namespace Flashy.API
     { 
         public static void Map(WebApplication app)
         {
+            app.MapPost("/checkuser", async (FlashyDbContext db, UserAuthDto userAuthDto) =>
+            {
+                var userUid = await db.Users.SingleOrDefaultAsync(u => u.Uid == userAuthDto.Uid);
+
+                if (userUid == null)
+                {
+                    return Results.NotFound();
+                }
+                else
+                {
+                    return Results.Ok();
+                }
+            });
+
+            app.MapPost("/register", async (FlashyDbContext db, User user) =>
+            {
+                var userInDb = await db.Users.SingleOrDefaultAsync(u => u.Uid == user.Uid);
+
+                if (userInDb != null)
+                {
+                    return Results.BadRequest("User already exists");
+                }
+                else
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return Results.Created($"/user/{user.Id}", user);
+                }
+            });
         }
     }
 }
