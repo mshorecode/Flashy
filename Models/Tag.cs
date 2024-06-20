b0043413c3adc7a192054e31a8c037aa6e9d3e31
Ryan Shore <mshorecode@gmail.com>
2024-06-19 20:18:58 -0500

    added userid to Tag

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -5,6 +5,7 @@ namespace Flashy.Models
     public class Tag
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         [Required]
         public string Label { get; set; }
         public ICollection<Flashcard> Flashcards { get; set; }
